4453bb2b47b9df029a217be30946c8eee4a07a8f
Halil İbrahim Gedik <halilgedik4234@gmail.com>
2022-03-14 10:48:38 +0300

    Example Passparola Game

--- Passaparola/Form1.Designer.cs
+++ Passaparola/Form1.Designer.cs
@@ -33,11 +33,11 @@
             this.button4 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
-            this.button6 = new System.Windows.Forms.Button();
             this.button7 = new System.Windows.Forms.Button();
-            this.button8 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button24 = new System.Windows.Forms.Button();
             this.button9 = new System.Windows.Forms.Button();
-            this.button10 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
             this.button11 = new System.Windows.Forms.Button();
             this.button12 = new System.Windows.Forms.Button();
             this.button13 = new System.Windows.Forms.Button();
@@ -51,7 +51,7 @@
             this.button21 = new System.Windows.Forms.Button();
             this.button22 = new System.Windows.Forms.Button();
             this.button23 = new System.Windows.Forms.Button();
-            this.button24 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
             this.button25 = new System.Windows.Forms.Button();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
@@ -115,32 +115,32 @@
             this.button5.Text = "E";
             this.button5.UseVisualStyleBackColor = true;
             // 
-            // button6
-            // 
-            this.button6.Location = new System.Drawing.Point(330, 132);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(47, 34);
-            this.button6.TabIndex = 6;
-            this.button6.Text = "G";
-            this.button6.UseVisualStyleBackColor = true;
-            // 
             // button7
             // 
-            this.button7.Location = new System.Drawing.Point(330, 92);
+            this.button7.Location = new System.Drawing.Point(330, 132);
             this.button7.Name = "button7";
             this.button7.Size = new System.Drawing.Size(47, 34);
-            this.button7.TabIndex = 5;
-            this.button7.Text = "F";
+            this.button7.TabIndex = 6;
+            this.button7.Text = "G";
             this.button7.UseVisualStyleBackColor = true;
             // 
-            // button8
+            // button6
             // 
-            this.button8.Location = new System.Drawing.Point(277, 252);
-            this.button8.Name = "button8";
-            this.button8.Size = new System.Drawing.Size(47, 34);
-            this.button8.TabIndex = 9;
-            this.button8.Text = "J";
-            this.button8.UseVisualStyleBackColor = true;
+            this.button6.Location = new System.Drawing.Point(330, 92);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(47, 34);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "F";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button24
+            // 
+            this.button24.Location = new System.Drawing.Point(118, 12);
+            this.button24.Name = "button24";
+            this.button24.Size = new System.Drawing.Size(47, 34);
+            this.button24.TabIndex = 9;
+            this.button24.Text = "Z";
+            this.button24.UseVisualStyleBackColor = true;
             // 
             // button9
             // 
@@ -151,140 +151,140 @@
             this.button9.Text = "I";
             this.button9.UseVisualStyleBackColor = true;
             // 
-            // button10
+            // button8
             // 
-            this.button10.Location = new System.Drawing.Point(330, 172);
-            this.button10.Name = "button10";
-            this.button10.Size = new System.Drawing.Size(47, 34);
-            this.button10.TabIndex = 7;
-            this.button10.Text = "H";
-            this.button10.UseVisualStyleBackColor = true;
+            this.button8.Location = new System.Drawing.Point(330, 172);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(47, 34);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "H";
+            this.button8.UseVisualStyleBackColor = true;
             // 
             // button11
             // 
-            this.button11.Location = new System.Drawing.Point(224, 252);
+            this.button11.Location = new System.Drawing.Point(277, 252);
             this.button11.Name = "button11";
             this.button11.Size = new System.Drawing.Size(47, 34);
             this.button11.TabIndex = 12;
-            this.button11.Text = "K";
+            this.button11.Text = "J";
             this.button11.UseVisualStyleBackColor = true;
             // 
             // button12
             // 
-            this.button12.Location = new System.Drawing.Point(171, 252);
+            this.button12.Location = new System.Drawing.Point(224, 252);
             this.button12.Name = "button12";
             this.button12.Size = new System.Drawing.Size(47, 34);
             this.button12.TabIndex = 11;
-            this.button12.Text = "L";
+            this.button12.Text = "K";
             this.button12.UseVisualStyleBackColor = true;
             // 
             // button13
             // 
-            this.button13.Location = new System.Drawing.Point(118, 252);
+            this.button13.Location = new System.Drawing.Point(171, 252);
             this.button13.Name = "button13";
             this.button13.Size = new System.Drawing.Size(47, 34);
             this.button13.TabIndex = 10;
-            this.button13.Text = "M";
+            this.button13.Text = "L";
             this.button13.UseVisualStyleBackColor = true;
             // 
             // button14
             // 
-            this.button14.Location = new System.Drawing.Point(65, 252);
+            this.button14.Location = new System.Drawing.Point(118, 252);
             this.button14.Name = "button14";
             this.button14.Size = new System.Drawing.Size(47, 34);
             this.button14.TabIndex = 15;
-            this.button14.Text = "N";
+            this.button14.Text = "M";
             this.button14.UseVisualStyleBackColor = true;
             // 
             // button15
             // 
-            this.button15.Location = new System.Drawing.Point(12, 252);
+            this.button15.Location = new System.Drawing.Point(65, 252);
             this.button15.Name = "button15";
             this.button15.Size = new System.Drawing.Size(47, 34);
             this.button15.TabIndex = 14;
-            this.button15.Text = "O";
+            this.button15.Text = "N";
             this.button15.UseVisualStyleBackColor = true;
             // 
             // button16
             // 
-            this.button16.Location = new System.Drawing.Point(12, 212);
+            this.button16.Location = new System.Drawing.Point(12, 250);
             this.button16.Name = "button16";
             this.button16.Size = new System.Drawing.Size(47, 34);
             this.button16.TabIndex = 13;
-            this.button16.Text = "P";
+            this.button16.Text = "O";
             this.button16.UseVisualStyleBackColor = true;
             // 
             // button17
             // 
-            this.button17.Location = new System.Drawing.Point(12, 172);
+            this.button17.Location = new System.Drawing.Point(12, 210);
             this.button17.Name = "button17";
             this.button17.Size = new System.Drawing.Size(47, 34);
             this.button17.TabIndex = 19;
-            this.button17.Text = "R";
+            this.button17.Text = "P";
             this.button17.UseVisualStyleBackColor = true;
             // 
             // button18
             // 
-            this.button18.Location = new System.Drawing.Point(12, 132);
+            this.button18.Location = new System.Drawing.Point(12, 170);
             this.button18.Name = "button18";
             this.button18.Size = new System.Drawing.Size(47, 34);
             this.button18.TabIndex = 18;
-            this.button18.Text = "S";
+            this.button18.Text = "R";
             this.button18.UseVisualStyleBackColor = true;
             // 
             // button19
             // 
-            this.button19.Location = new System.Drawing.Point(12, 92);
+            this.button19.Location = new System.Drawing.Point(12, 130);
             this.button19.Name = "button19";
             this.button19.Size = new System.Drawing.Size(47, 34);
             this.button19.TabIndex = 17;
-            this.button19.Text = "T";
+            this.button19.Text = "S";
             this.button19.UseVisualStyleBackColor = true;
             // 
             // button20
             // 
-            this.button20.Location = new System.Drawing.Point(12, 52);
+            this.button20.Location = new System.Drawing.Point(12, 90);
             this.button20.Name = "button20";
             this.button20.Size = new System.Drawing.Size(47, 34);
             this.button20.TabIndex = 16;
-            this.button20.Text = "U";
+            this.button20.Text = "T";
             this.button20.UseVisualStyleBackColor = true;
             // 
             // button21
             // 
-            this.button21.Location = new System.Drawing.Point(12, 12);
+            this.button21.Location = new System.Drawing.Point(12, 50);
             this.button21.Name = "button21";
             this.button21.Size = new System.Drawing.Size(47, 34);
             this.button21.TabIndex = 21;
-            this.button21.Text = "V";
+            this.button21.Text = "U";
             this.button21.UseVisualStyleBackColor = true;
             // 
             // button22
             // 
-            this.button22.Location = new System.Drawing.Point(65, 12);
+            this.button22.Location = new System.Drawing.Point(12, 12);
             this.button22.Name = "button22";
             this.button22.Size = new System.Drawing.Size(47, 34);
             this.button22.TabIndex = 20;
-            this.button22.Text = "Y";
+            this.button22.Text = "V";
             this.button22.UseVisualStyleBackColor = true;
             // 
             // button23
             // 
-            this.button23.Location = new System.Drawing.Point(118, 12);
+            this.button23.Location = new System.Drawing.Point(65, 12);
             this.button23.Name = "button23";
             this.button23.Size = new System.Drawing.Size(47, 34);
             this.button23.TabIndex = 22;
-            this.button23.Text = "Z";
+            this.button23.Text = "Y";
             this.button23.UseVisualStyleBackColor = true;
             // 
-            // button24
+            // button10
             // 
-            this.button24.Location = new System.Drawing.Point(330, 252);
-            this.button24.Name = "button24";
-            this.button24.Size = new System.Drawing.Size(47, 34);
-            this.button24.TabIndex = 23;
-            this.button24.Text = "İ";
-            this.button24.UseVisualStyleBackColor = true;
+            this.button10.Location = new System.Drawing.Point(330, 252);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(47, 34);
+            this.button10.TabIndex = 23;
+            this.button10.Text = "İ";
+            this.button10.UseVisualStyleBackColor = true;
             // 
             // button25
             // 
@@ -327,10 +327,11 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(69, 36);
+            this.textBox1.Location = new System.Drawing.Point(44, 40);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(280, 26);
             this.textBox1.TabIndex = 0;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
             // 
             // groupBox3
             // 
@@ -419,12 +420,13 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SandyBrown;
             this.ClientSize = new System.Drawing.Size(754, 422);
             this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.button25);
-            this.Controls.Add(this.button24);
+            this.Controls.Add(this.button10);
             this.Controls.Add(this.button23);
             this.Controls.Add(this.button21);
             this.Controls.Add(this.button22);
@@ -438,11 +440,11 @@
             this.Controls.Add(this.button11);
             this.Controls.Add(this.button12);
             this.Controls.Add(this.button13);
-            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button24);
             this.Controls.Add(this.button9);
-            this.Controls.Add(this.button10);
-            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button8);
             this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
@@ -469,11 +471,11 @@
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.Button button6;
         private System.Windows.Forms.Button button7;
-        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button24;
         private System.Windows.Forms.Button button9;
-        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button8;
         private System.Windows.Forms.Button button11;
         private System.Windows.Forms.Button button12;
         private System.Windows.Forms.Button button13;
@@ -487,7 +489,7 @@
         private System.Windows.Forms.Button button21;
         private System.Windows.Forms.Button button22;
         private System.Windows.Forms.Button button23;
-        private System.Windows.Forms.Button button24;
+        private System.Windows.Forms.Button button10;
         private System.Windows.Forms.Button button25;
         private System.Windows.Forms.RichTextBox richTextBox1;
         private System.Windows.Forms.GroupBox groupBox1;
--- Passaparola/Form1.cs
+++ Passaparola/Form1.cs
@@ -17,7 +17,138 @@ namespace Passaparola
             InitializeComponent();
         }
 
-        int soruno = 0, dogru = 0, yanlıs = 0;
+        int soruno = 0, dogru = 0, yanlis = 0;
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                switch (soruno)
+                {
+                    case 1:
+                        if (textBox1.Text == "akdeniz")
+                        {
+                            button1.BackColor = Color.Green;
+                            dogru++;
+                            LblDogruSayısı.Text = dogru.ToString();
+                        }
+                        else
+                        {
+                            button1.BackColor = Color.Red;
+                            yanlis++;
+                            LabelYanlısSayısı.Text=yanlis.ToString();
+                        }
+                        break;
+                    case 2:
+                        if (textBox1.Text == "bursa") 
+                        {
+                            button2.BackColor = Color.Green;
+                            dogru++;
+                            LblDogruSayısı.Text = dogru.ToString();
+                        }
+                        else
+                        {
+                            button2.BackColor= Color.Red;
+                            yanlis++;
+                            LabelYanlısSayısı.Text = yanlis.ToString();
+                        }
+                        break;
+
+                    case 3:
+                        if (textBox1.Text == "cuma")
+                        {
+                            button3.BackColor = Color.Green;
+                            dogru++;
+                            LblDogruSayısı.Text = dogru.ToString();
+                        }
+                        else
+                        {
+                            button3.BackColor = Color.Red;
+                            yanlis++;
+                            LabelYanlısSayısı.Text = yanlis.ToString();
+                        }
+                        break;
+
+                    case 4:
+                        if (textBox1.Text == "diyarbakır")
+                        {
+                            button4.BackColor = Color.Green;
+                            dogru++;
+                            LblDogruSayısı.Text = dogru.ToString();
+                        }
+                        else
+                        {
+                            button4.BackColor = Color.Red;
+                            yanlis++;
+                            LabelYanlısSayısı.Text = yanlis.ToString();
+                        }
+                        break ;
+
+                    case 5:
+                        if (textBox1.Text == "eski")
+                        {
+                            button5.BackColor = Color.Green;
+                            dogru++;
+                            LblDogruSayısı.Text = dogru.ToString();
+                        }
+                        else
+                        {
+                            button5.BackColor = Color.Red;
+                            yanlis++;
+                            LabelYanlısSayısı.Text = yanlis.ToString();
+                        }
+                        break;
+
+                    case 6:
+                        if (textBox1.Text == "ferman")
+                        {
+                            button6.BackColor = Color.Green;
+                            dogru++;
+                            LblDogruSayısı.Text = dogru.ToString();
+                        }
+                        else
+                        {
+                            button6.BackColor = Color.Red;
+                            yanlis++;
+                            LabelYanlısSayısı.Text = yanlis.ToString();
+                        }
+                        break;
+
+                    case 7:
+                        if (textBox1.Text == "güneş")
+                        {
+                            button7.BackColor = Color.Green;
+                            dogru++;
+                            LblDogruSayısı.Text = dogru.ToString();
+                        }
+                        else
+                        {
+                            button7.BackColor = Color.Red;
+                            yanlis++;
+                            LabelYanlısSayısı.Text = yanlis.ToString();
+                        }
+                        break;
+                    case 8:
+                        if (textBox1.Text == "çalışkan")
+                        {
+                            button8.BackColor = Color.Green;
+                            dogru++;
+                            LblDogruSayısı.Text = dogru.ToString();
+                        }
+                        else
+                        {
+                            button8.BackColor = Color.Red;
+                            yanlis++;
+                            LabelYanlısSayısı.Text = yanlis.ToString();
+
+                        }
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+        }
 
         private void Form1_Load(object sender, EventArgs e)
         {
@@ -26,7 +157,7 @@ namespace Passaparola
 
         private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
-            linkLabel1.Text = "CEVAPLA";
+            linkLabel1.Text = "Sonraki";
             soruno++;
             this.Text = soruno.ToString();
             lblSoruNoSayısı.Text = soruno.ToString();
@@ -58,15 +189,103 @@ namespace Passaparola
                     button5.BackColor = Color.Yellow;
                     break;
 
-                case 7:
+                case 6:
                     richTextBox1.Text = " Padişahların emirlerinin yazılı haline ne denir ?";
+                    button6.BackColor = Color.Yellow;
+                    break;
+
+                case 7:
+                    richTextBox1.Text = " Dünyamızın Isı Kaynağı ?";
                     button7.BackColor = Color.Yellow;
                     break;
 
-                case 6:
-                    richTextBox1.Text = " Dünyamızın Isı Kaynağı ?";
-                    button6.BackColor = Color.Yellow;
+                case 8:
+                    richTextBox1.Text = " Öğrenciler arasında 'İnek' tabiri ne demektir ?";
+                    button8.BackColor = Color.Yellow;
                     break;
+
+                case 9:
+                    richTextBox1.Text = " Gülüyle ünlü ilimiz ?";
+                    button9.BackColor = Color.Yellow;
+                    break;
+
+                case 10:
+                    richTextBox1.Text = " Mersinin Diğer İsmi ?";
+                    button10.BackColor = Color.Yellow;
+                    break;
+
+                case 11:
+                    richTextBox1.Text = " Havada en çok bulunuan bileşen ?";
+                    button11.BackColor = Color.Yellow;
+                    break;
+
+                case 12:
+                    richTextBox1.Text = " Konyanın neyi ile ünlüdür ?";
+                    button12.BackColor = Color.Yellow;
+                    break;
+
+                case 13:
+                    richTextBox1.Text = " Her yıl Bahar aylarında düzenlenen meşhur çicek festivali  ?";
+                    button13.BackColor = Color.Yellow;
+                    break;
+
+                case 14:
+                    richTextBox1.Text = " Konyanın neyi ile ünlüdür ?";
+                    button14.BackColor = Color.Yellow;
+                    break;
+
+                case 15:
+                    richTextBox1.Text = " 3. ayın ingilizcesi nedir ?";
+                    button15.BackColor = Color.Yellow;
+                    break;
+
+                case 16:
+                    richTextBox1.Text = " Üflemeli Müzik Aleti ?";
+                    button16.BackColor = Color.Yellow;
+                    break;
+
+                case 17:
+                    richTextBox1.Text = " Çocukların çok sevmediği pirinç,havuç gibi şeylerden yapılan yemek ?";
+                    button17.BackColor = Color.Yellow;
+                    break;
+
+                case 18:
+                    richTextBox1.Text = " 11 ayın sultanı diye hitap edilen ay nedir  ?";
+                    button18.BackColor = Color.Yellow;
+                    break;
+
+                case 19:
+                    richTextBox1.Text = " İngilizce 'What is he/she like ? ' ne anlama gelmektedir ";
+                    button19.BackColor = Color.Yellow;
+                    break;
+
+                case 20:
+                    richTextBox1.Text = " Türkiyenin cumhurbaşkanı ?";
+                    button20.BackColor = Color.Yellow;
+                    break;
+
+                case 21:
+                    richTextBox1.Text = " Ümit kelimesinin eş anlamlısı nedir ?";
+                    button21.BackColor = Color.Yellow;
+                    break;
+
+                case 22:
+                    richTextBox1.Text = " Kahvaltısı ile ünlü ilimiz ?";
+                    button22.BackColor = Color.Yellow;
+                    break;
+
+                case 23:
+                    richTextBox1.Text = " Şimşek kelimesinin eş anlamlısı ?";
+                    button23.BackColor = Color.Yellow;
+                    break;
+
+                case 24:
+                    richTextBox1.Text = " Ege bölgesinin en çok ağacı bulunan yağıda yapılan bir kahvaltı besini ?";
+                    button24.BackColor = Color.Yellow;
+                    break;
+
+
+
                 default:
                     break;
             }

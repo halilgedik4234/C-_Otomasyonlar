6bd3be73436e05a96ed52b180c390f079c8545e8
Halil İbrahim Gedik <100279727+halilgedik4234@users.noreply.github.com>
2023-01-16 21:24:52 +0300

    Quiz Console Application added

---
+++ Quiz_Uygulaması/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Quiz_Uygulaması
+{
+    class Question
+    {
+        public Question(string text, string[] choices,string answer)
+        {
+            this.Text = text;
+            this.Choices = choices;
+            this.Answer = answer;
+        }
+
+        public string Text { get; set; }
+
+        public string[] Choices { get; set; }
+
+        public string Answer { get; set; }
+
+        public bool checkAnswer(string answer)
+        {
+            return this.Answer.ToLower() == answer.ToLower();
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var q1 = new Question("En iyi programlama dili nedir ?", new string[] { "C#", "Javascript", "java", "Python" }, "C#");
+
+            var q2 = new Question("En popüler programlama dili nedir ?", new string[] { "Python", "Javascript", "C#", "Java" }, "C#");
+
+            var q3 = new Question("En çok kazandıran programlama dili nedir ?", new string[] { "JavaScript", "C#", "java", "Python" }, "C#");
+
+            
+            Question[] questions = new Question[]{ q1, q2, q3 };
+
+            var quiz=new Quiz(questions);
+
+            quiz.DisplayQuestions();
+
+        }
+    }
+}
---
+++ Quiz_Uygulaması/Quiz.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Quiz_Uygulaması
+{
+    internal class Quiz
+    {
+        public Quiz(Question[] questions)
+        {
+            this.Questions = questions;
+            this.QuestionIndex = 0;
+            this.Score = 0;
+        }
+
+        private Question[] Questions { get; set; }
+
+        private int QuestionIndex { get; set; }
+
+        private int Score { get; set; }
+
+        private Question GetQuestion()
+        {
+            return this.Questions[this.QuestionIndex];
+
+        }
+
+        public void DisplayQuestions()
+        {
+            var question = this.GetQuestion();
+            this.DisplayProgress();
+            Console.WriteLine($"soru {this.QuestionIndex + 1}: {question.Text}");
+
+            foreach (var a in question.Choices)
+            {
+                Console.WriteLine($" - {a}");
+            }
+
+            Console.Write("\nCevap: ");
+
+            string cevap = Console.ReadLine();
+            this.Guess(cevap);
+        }
+
+        private void Guess(string answer)
+        {
+            var question = this.GetQuestion();
+            if (question.checkAnswer(answer))
+                this.Score++;  //skor
+
+            this.QuestionIndex++;
+
+            if (this.Questions.Length == this.QuestionIndex)
+            {
+                //skor gösterme oyun bitince
+                this.DisplayScore();
+                PlayAgain();
+                return;
+            }
+            else
+            {
+                this.DisplayQuestions();
+            }
+        }
+
+        private void DisplayScore()
+        {
+            if (Score == Questions.Length)
+                Console.WriteLine("Your Score 100");
+            else
+                Console.WriteLine($"Your Score: {(100 / Questions.Length) * this.Score} ");
+        }
+
+        private void DisplayProgress()
+        {
+            int totalQuestion = this.Questions.Length;
+            int questionNumber = this.QuestionIndex + 1;
+            if (totalQuestion >= questionNumber)
+            {
+                Console.WriteLine($"Question: {questionNumber}/{totalQuestion}");
+            }
+        }
+
+        private void PlayAgain()
+        {
+            Console.WriteLine("Tekrar Oynamak İster misiniz ? (e/h)");
+            string durum = Console.ReadLine();
+            if (durum == "e" || durum == "evet")
+            {
+                this.QuestionIndex = 0;
+                this.Score = 0;
+                DisplayQuestions();
+            }
+            else
+            {
+                Console.WriteLine("Oyun kapanıyor");
+            }
+        }
+    }
+}
